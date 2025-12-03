d0b0667d56e55d4152f0b0af6163ac6aab3e4564
luciano-antonio-rowlands-carvalho <luciano.antonio.rowlands.carvalho@gmail.com>
2025-12-03 13:31:55 -0300

    Configure GitHub Pages deploy with CNAME

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/',   // domínio personalizado exige isso
   plugins: [
     vue(),
     tailwindcss(),
